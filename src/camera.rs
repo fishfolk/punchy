936fdbd299524a14a2922509a5df4452c1bf98a3
Saverio Miroddi <saverio.pub2@gmail.com>
2022-07-16 01:01:16 +0200

    Fix first crash on players death (in camera_follow_player())

--- src/camera.rs
+++ src/camera.rs
@@ -77,20 +77,21 @@ pub fn camera_follow_player(
     let max_player_x = player_query
         .iter()
         .map(|transform| transform.translation.x)
-        .max_by(|ax, bx| ax.total_cmp(bx))
-        .unwrap();
+        .max_by(|ax, bx| ax.total_cmp(bx));
 
-    let (mut camera, panning) = camera_query.single_mut();
+    if let Some(max_player_x) = max_player_x {
+        let (mut camera, panning) = camera_query.single_mut();
 
-    let max_player_x_diff =
-        max_player_x - camera.translation.x - game_meta.camera_move_right_boundary;
+        let max_player_x_diff =
+            max_player_x - camera.translation.x - game_meta.camera_move_right_boundary;
 
-    if max_player_x_diff > 0. {
-        // The x axis is handled by the parallax plugin.
-        camera.translation.y = consts::GROUND_Y + panning.offset.y;
+        if max_player_x_diff > 0. {
+            // The x axis is handled by the parallax plugin.
+            camera.translation.y = consts::GROUND_Y + panning.offset.y;
 
-        move_event_writer.send(ParallaxMoveEvent {
-            camera_move_speed: max_player_x_diff * consts::CAMERA_SPEED,
-        });
+            move_event_writer.send(ParallaxMoveEvent {
+                camera_move_speed: max_player_x_diff * consts::CAMERA_SPEED,
+            });
+        }
     }
 }
